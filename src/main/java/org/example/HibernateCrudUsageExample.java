99b70d973fe5b7634c5cb0f59939a6ba0b5f7ad4
illia-pastushenko <nzxt511@gmail.com>
2023-11-22 23:10:27 +0100

    initial commit

---
+++ src/main/java/org/example/HibernateCrudUsageExample.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class HibernateCrudUsageExample {
+    public static void main(String[] args) {
+        PersonService personService = new PersonService();
+
+        Person newPerson = new Person();
+        newPerson.setName("John Doe");
+        personService.savePerson(newPerson);
+
+        Person person = personService.findPersonById(1L);
+        System.out.println("Found Person: " + person.getName());
+
+        person.setName("Jane Doe");
+        personService.updatePerson(person);
+
+        personService.deletePerson(person);
+
+        HibernateUtil.getInstance().close();
+    }
+}
